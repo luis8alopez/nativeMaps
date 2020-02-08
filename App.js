f8bfad1edabb117d7864a53778e999a7c045cdda
Luis <eduardo.leo8a@hotmail.com>
2020-02-08 14:13:32 -0500

    adding login screen and linear gradient

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 import FindMe from './components/FindMe';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import Header from './components/Header';
+
 import MapaScreen from './screens/MapaScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './components/Login';
 import PlacesScreen from './screens/Places';
+import AuthScreen from './screens/AuthScreen';
 
-import { withAuthenticator } from 'aws-amplify-react-native';
 import Amplify from '@aws-amplify/core';
 import config from './aws-exports';
 Amplify.configure(config);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
-  
-  const [vista, setVista] = useState(0);
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+  const [vista, setVista] = useState(5);
   const [loc, setLoc] = useState();
   const [long, setLong] = useState(0);
   const [lat, setLat] = useState(0);
@@ -33,38 +44,52 @@ export default function App() {
     setLat(latitude);
   };
 
-  const vistaHandler = vis =>{
+  const vistaHandler = vis => {
     setVista(vis);
   }
 
 
-  let ventana = <HomeScreen/>
-  //let ventana = <LoginScreen/>
-  //let ventana = <PlacesScreen/>  
+  let ventana = <AuthScreen />
 
-  if(vista===0){
-    ventana = <HomeScreen onVista={vistaHandler}/>
+
+  if (vista === 0) {
+    ventana = <HomeScreen onVista={vistaHandler} />
   }
-  if(vista === 1){
-    ventana = <MapaScreen onLon={long} onLat={lat}/>
+  if (vista === 1) {
+    ventana = <MapaScreen onLon={long} onLat={lat} />
   }
-  if(vista ===2){
-    ventana = <FindMe onGetLoc={locationHandler} onGetLong={longHandler} onGetLat={latHandler} onVista={vistaHandler}/>
+  if (vista === 2) {
+    ventana = <FindMe onGetLoc={locationHandler} onGetLong={longHandler} onGetLat={latHandler} onVista={vistaHandler} />
   }
-  if(vista ===3){
-    ventana = <LoginScreen onVista={vistaHandler}/>
+  if (vista === 3) {
+    ventana = <LoginScreen onVista={vistaHandler} />
   }
-  if(vista ===4){
-    ventana = <PlacesScreen/>
+  if (vista === 4) {
+    ventana = <PlacesScreen />
   }
-  
+  if (vista === 5) {
+    ventana = <AuthScreen />
+  }
+  if (!fontLoaded) {
     return (
-      <View style={styles.container}>
-        <Header title="Native Maps"/> 
-        {ventana}
-      </View>
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
     );
-  
+  }
+
+  return (
+    <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
+    <View style={styles.container}>
+      {/* <Header title="Native Maps"/>  */}
+      {ventana}
+    </View>
+    </LinearGradient>
+  );
+
 }
 
 const styles = StyleSheet.create({
@@ -73,6 +98,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  gradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
-  
+
