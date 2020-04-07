26eca1f8c0f6deae72da2d6b826b503693b4db26
Luis <eduardo.leo8a@hotmail.com>
2020-04-07 17:26:05 -0500

    adding some useEffect logic

--- screens/RefundScreen.js
+++ screens/RefundScreen.js
@@ -1,82 +1,116 @@
-import React, { Component } from 'react'; 
-import { ListView, Text, View, Image, StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ListView, Text, View, Image, StyleSheet, FlatList, Alert } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  img: {
-    width: 240,
-    height: 110,
-    marginLeft:20
-  },
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
+    img: {
+        width: 240,
+        height: 110,
+        marginLeft: 20
+    },
 });
 
-const data = [
+let data = [
     {
-        id: '1000',
-        title: 'Mil',
-        image: require('../assets/rsz_1000.jpg')
+        id: '100000',
+        title: 'Cienmil',
+        image: require('../assets/rsz_100000.jpg')
     },
     {
-        id: '2000',
-        title: 'Dos mil',
-        image: require('../assets/rsz_2000.jpg')
+        id: '50000',
+        title: 'Cincuentamil',
+        image: require('../assets/rsz_50000.jpg')
+    },
+    {
+        id: '20000',
+        title: 'Veintemil',
+        image: require('../assets/rsz_20000.png')
+    },
+    {
+        id: '10000',
+        title: 'Diezmil',
+        image: require('../assets/rsz_110000.jpg')
     },
     {
         id: '5000',
-        title: 'Cinco mil',
+        title: 'CincoMil',
         image: require('../assets/rsz_5000.jpg')
     },
     {
-      id: '10000',
-      title: 'Diez mil',
-      image: require('../assets/rsz_110000.jpg')
+        id: '2000',
+        title: 'Dosmil',
+        image: require('../assets/rsz_2000.jpg')
     },
     {
-      id: '20000',
-      title: 'Veinte Mil',
-      image: require('../assets/rsz_20000.png')
+        id: '1000',
+        title: 'mil',
+        image: require('../assets/rsz_1000.jpg')
     },
-    {
-      id: '50000',
-      title: 'Cincuenta mil',
-      image: require('../assets/rsz_50000.jpg')
-    },
-    {
-      id: '100000',
-      title: 'Cien mil',
-      image: require('../assets/rsz_100000.jpg')
-    },
-  ];
+];
 
 
-class RefundScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  //Lógica acá que luego de llamar al Servicio de devuelta, coja el JSON
-  // Y vea qué ids trajo y luego crear subArray de data para mostrar en el FlatList
-  
+RefundScreen = props => {
 
-  render() {
+    const [refund, setRefund] = useState([]);
+
+    callApi = async () => {
+        //Cambiar price para que se reciba dinámicamente cuando el navigation de maps
+        //Apunte a esta Screen --- Tal vez Redux?
+        return await axios('https://refunding-backend.herokuapp.com/api/getRefund?price=15000')
+            .then((response) => {
+                return response;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        const retorno = async () => {
+            const reto = await callApi();
+            if (!reto) {
+                console.log("Something went wrong");
+            }
+            setRefund(String(reto.data.refund.refund));
+            //Acá se puede hacer machetazo pa llamar función que cambie el array data para mostrar
+            let num = ["100000","50000","20000","10000","5000","2000","1000"];
+
+            for(let i=0;i<7;i++){
+                let help = num[i];
+                if(reto.data.refund.refund[help]){
+                    let jsonObj= {};
+                    jsonObj["id"] = help;
+                    jsonObj["quantity"] = reto.data.refund.refund[help];
+                    //JsonObj o en su defecto --help-- tiene el id que se debería borrar en data[] para hacer el render
+                    // cómo?
+                }               
+            }
+        }
+        retorno();
+    }, []);
+
     return (
-      <FlatList
-        data={data}
-        keyExtractor={(item, index) => item.id}
-        renderItem={({item}) => (
-          <View style={styles.container}>
-            <Image
-              source={item.image}
-              style={styles.img}
+        <LinearGradient colors={['#005AA7', '#FFFDE4']} style={styles.gradient}>
+            <FlatList
+                data={data}
+                keyExtractor={(item, index) => item.id}
+                renderItem={({ item }) => (
+                    <View style={styles.container}>
+                        <Image
+                            source={item.image}
+                            style={styles.img}
+                        />
+                        <Text>{item.id}</Text>
+                    </View>)}
             />
-            <Text>{item.id}</Text>
-          </View>)}
-      />
+        </LinearGradient>
     );
-  }
 }
 
 export default RefundScreen;
